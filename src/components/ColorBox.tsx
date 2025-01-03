e5a5b7464416dc550bcda08f2533e99db06366b2
sina-byn <sina.sina.bayan@protonmail.com>
2025-01-03 14:22:43 +0330

    refactor: bottomCenter util added and color prop type updated

--- src/components/ColorBox.tsx
+++ src/components/ColorBox.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Box, Text, type TextProps } from 'ink';
+import { Box, Text } from 'ink';
 
 // *  utils
 const spaces = (count: number = 10) => Array(count).fill(' ').join('');
 
+const bottomCenter = (text: string) => spaces(7) + text + spaces(2);
+
 // * types
-type Color = TextProps['backgroundColor'];
+import type { Color } from '../index.js';
 
 type ColorProps = { color: Color; active?: boolean };
 
@@ -13,9 +15,7 @@ const ColorBox = ({ color, active }: ColorProps) => {
   return (
     <Box width={5} height={4}>
       {active ? (
-        <Text backgroundColor={color}>
-          {spaces(7)}&#9679;{spaces(2)}
-        </Text>
+        <Text backgroundColor={color}>{bottomCenter('\u25CF')}</Text>
       ) : (
         <Text backgroundColor={color}>{spaces()}</Text>
       )}
